0d6d1a5cc3188dd45baf2b2a87ef680b19320632
mahmut559 <mahmut559@RPC03>
2024-03-14 14:06:25 +0300

    Proje dosyası ekle.

---
+++ uygulama13/Ogrenciler.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace uygulama13
+{
+
+    public class Ogrenciler
+    {
+        public int Numara { get; set; }
+
+        public string AdSoyad { get; set; }
+
+        public bool cinsiyet { get; set; }
+    }
+}
+
